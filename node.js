cf513d9441f67af31ef8002a51959da12e114cec
lucasjsr2 <lucasjuan14@gmail.com>
2023-05-30 21:52:18 -0300

    Bump dependencies

--- node.js
+++ node.js
@@ -1,32 +1,32 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-  },
-  extends: ['standard', 'plugin:prettier/recommended'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        printWidth: 80,
-        tabWidth: 4,
-        singleQuote: false,
-        trailingComma: 'all',
-        arrowParens: 'always',
-        semi: true,
-        endOfLine: 'auto',
-      },
-    ],
-  },
-  settings: {
-    'import/parsers': {
-      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    env: {
+        es2021: true,
+        node: true,
     },
-  },
+    extends: ['standard', 'plugin:prettier/recommended'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+        'prettier/prettier': [
+            'error',
+            {
+                printWidth: 80,
+                tabWidth: 4,
+                singleQuote: false,
+                trailingComma: 'all',
+                arrowParens: 'always',
+                semi: true,
+                endOfLine: 'auto',
+            },
+        ],
+    },
+    settings: {
+        'import/parsers': {
+            [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+        },
+    },
 }
